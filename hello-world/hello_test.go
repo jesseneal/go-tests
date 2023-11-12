7a3328ec27e7a477cb7a7ed51d6b6875f3e9497d
jesseneal <jesse.neal@hashicorp.com>
2023-11-12 12:33:22 -0600

    hello world

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Jesse")
-	want := "Hello, Jesse"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Jesse")
+		want := "Hello, Jesse"
+		assertCorretctMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorretctMessage(t, got, want)
+	})
 
+}
+
+func assertCorretctMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+
 }
