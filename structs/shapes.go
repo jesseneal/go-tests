4f29da4ebd05833fa03bf67a2ee9d014ff4d4f25
jesseneal <jesse.neal@hashicorp.com>
2024-01-27 16:43:45 -0600

    added interfaces

--- structs/shapes.go
+++ structs/shapes.go
@@ -7,7 +7,6 @@ type Rectangle struct {
 	Height float64
 }
 
-
 type Circle struct {
 	Radius float64
 }
@@ -28,7 +27,6 @@ func (r Rectangle) Area() float64 {
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
-dd
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
